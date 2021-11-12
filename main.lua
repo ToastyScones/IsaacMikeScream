68965a551536941199fbc9a649147d4406f016ac
Toasty Scones <fake@fake.com>
2021-11-12 01:52:40 -0600

    fix sound not playing on each hit

--- main.lua
+++ main.lua
@@ -3,22 +3,24 @@ local sfxManager = SFXManager()
 local mikehurt = Isaac.GetSoundIdByName("mikehurt")
 local mikedie = Isaac.GetSoundIdByName("mikedie")
 
+function mikecry:takeDmgCallback(target, damage, flags, source, damageCountdown)
+	local player = Isaac.GetPlayer(0)
+	if target.Type == EntityType.ENTITY_PLAYER then
+		sfxManager:Play(mikehurt, 1.0, 0, false, 1.0)
+	end
+end
+
 function mikecry:hurtCallback()
 	local isIsaacHurtPlaying = sfxManager:IsPlaying(SoundEffect.SOUND_ISAAC_HURT_GRUNT)
-	local isMikeHurtPlaying = sfxManager:IsPlaying(mikehurt)
-
 	if (isIsaacHurtPlaying) then
 		sfxManager:Stop(SoundEffect.SOUND_ISAAC_HURT_GRUNT)
-		if (not (isMikeHurtPlaying)) then 
-			sfxManager:Play(mikehurt, 1.0, 0, false, 1.0)
-		end
 	end
 end
 
 function mikecry:deathCallback()
 	local isIsaacDiesPlaying = sfxManager:IsPlaying(SoundEffect.SOUND_ISAACDIES)
 	local isMikeDiesPlaying = sfxManager:IsPlaying(mikedie)
-
+	Isaac.DebugString(tostring(mikedie))
 	if (isIsaacDiesPlaying) then
 		sfxManager:Stop(SoundEffect.SOUND_ISAACDIES)
 		if (not (isMikeDiesPlaying)) then 
@@ -27,5 +29,6 @@ function mikecry:deathCallback()
 	end
 end
 
+mikecry:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, mikecry.takeDmgCallback)
 mikecry:AddCallback(ModCallbacks.MC_POST_UPDATE, mikecry.hurtCallback);
 mikecry:AddCallback(ModCallbacks.MC_POST_UPDATE, mikecry.deathCallback);
